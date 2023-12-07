eedf7fb48bd8f3a473f41ff3691d410225fb4020
e2001024 <e2001024@edu.vamk.fi>
2023-12-07 15:37:59 +0200

    Cors fix

---
+++ backend/src/main/java/com/vamk/e2001024/studentmanager/security/RestConfig.java
@@ -0,0 +1,24 @@
+package com.vamk.e2001024.studentmanager.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Update with your frontend's URL or use '*' to allow all origins
+        config.addAllowedOrigin("http://localhost:80");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
