14d6a889c4e069aaa6f3dc178c2b7e256e22a352
e2001024 <e2001024@edu.vamk.fi>
2023-12-10 21:09:53 +0200

    updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,22 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './sites/login/login.component';
-import { CoursesComponent } from './sites/courses/courses.component';
-import { StudentsComponent } from './sites/students/students.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RegisterComponent } from './sites/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    StudentsComponent,
-    CoursesComponent,
-    CoursesComponent,
-    RegisterComponent,
   ],
   imports: [
     BrowserModule,
